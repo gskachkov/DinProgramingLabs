df602384a45d07a4080ae73a47090b250b9b1518
Skachov, Oleksandr <oleksandr.skachov@storebrand.no>
2014-03-30 21:44:52 +0200

    Coloring

--- coloring/Solver.js
+++ coloring/Solver.js
@@ -3,7 +3,7 @@ var path = require('path');
 var events = require("events");
 var printResult = require("./printResult");
 //var alogirtm = require("./algorithms/greedyAlgoritm");
-var alogirtm = require("./algorithms/branchAndBunchRelaxWithSort");
+var alogirtm = require("./algorithms/simple_1");
 
 process.argv.forEach(function (val, index, array) {
   if(val.indexOf("-file=")> -1) {
@@ -13,9 +13,8 @@ process.argv.forEach(function (val, index, array) {
 
 var lines= [];
 var values = [];
-var weights = [];
 var isFirst = true;
-var capacity;
+var length;
 
 var readFile = fs.readFileSync(fileName, { encoding: 'utf8' })
   .toString()
@@ -25,13 +24,16 @@ var readFile = fs.readFileSync(fileName, { encoding: 'utf8' })
 
     var parts = line.split(" ");
     if (isFirst) {
-      capacity = parseInt(parts[1]);
+      vertexesCount = parseInt(parts[0]);
+      adgesCount = parseInt(parts[1]);
       isFirst = false;
     } else {
-      values.push(parseInt(parts[0]));
-      weights.push(parseInt(parts[1]));
+      values.push({
+        a : parseInt(parts[0]),
+        b : parseInt(parts[1])
+      });
     }
 });
 
-var result = alogirtm (capacity, values, weights);
+var result = alogirtm (vertexesCount, adgesCount, values);
 printResult(result);
---
+++ coloring/algorithms/simple_1.js
@@ -0,0 +1,13 @@
+module.exports = function bruteForce (vertexesCount, adgesCount, values) {
+
+  var result = [];
+
+  for(var i=0; i < vertexesCount; i++) {
+    result.push(i);
+  }
+
+  return {
+    value: vertexesCount,
+    taken: result
+  };
+}
